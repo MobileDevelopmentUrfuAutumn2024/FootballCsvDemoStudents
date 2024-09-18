afc7b8e5d33f2133559bd53cee811128bd79d6a9
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-09-18 13:09:51 +0500

    create model

--- src/main/kotlin/model/Team.kt
+++ src/main/kotlin/model/Team.kt
@@ -1,3 +1,11 @@
 package model
 
-class Team
+/**
+ * Команда
+ * @param name имя команды
+ * @param players список игроков
+ */
+data class Team(
+    val name: String,
+    val players: List<Player>
+)
