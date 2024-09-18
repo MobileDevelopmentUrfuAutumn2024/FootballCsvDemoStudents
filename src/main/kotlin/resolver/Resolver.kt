da966d86ce2cb5d3435b452af20fb05d06378a29
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-09-18 13:10:14 +0500

    update resolver

--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,43 @@
 package resolver
 
-class Resolver
+import model.Player
+import model.Position
+import model.Team
+import model.getPosition
+import parser.CsvParser
+
+/**
+ * Решение задач
+ * @param path путь к файлу
+ */
+class Resolver(
+    path: String
+) : IResolver {
+    private val csvParser = CsvParser
+    val players: List<Player> = csvParser.parse(path)
+
+    override fun getCountWithoutAgency(): Int = players
+        .filter { it.agency.isNullOrEmpty() }.size
+
+    override fun getBestScorerDefender(): Pair<String, Int> {
+        val player = players
+            .filter { getPosition(it.position.name) == Position.DEFENDER }
+            .maxBy { it.goals }
+
+        return Pair(player.name, player.goals)
+    }
+
+    override fun getTheExpensiveGermanPlayerPosition(): String = getPosition(
+        players.maxBy { it.transferCost }.position.name
+    ).pos
+
+    override fun getTheRudestTeam(): Team {
+        val teams: MutableList<Team> = mutableListOf()
+
+        players
+            .groupBy { it.team }
+            .forEach { teams.add(Team(it.key, it.value)) }
+
+        return teams.maxBy { it.players.sumOf { players -> players.redCards } / it.players.count() }
+    }
+}
