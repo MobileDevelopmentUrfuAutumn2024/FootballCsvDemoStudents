98679d458189863d69ee978eb1336bf73e20c839
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2024-09-21 00:55:05 +0500

    feat: solve

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,37 @@
 package parser
 
-object CsvParser
+import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReaderBuilder
+import model.Player
+import model.Team
+import model.enums.Position
+import java.io.FileReader
+
+object CsvParser {
+    fun readCsvWithPlayers(path: String, separator: Char = ';'): Array<Player> {
+        val parser = CSVParserBuilder()
+            .withSeparator(separator)
+            .build()
+        return CSVReaderBuilder(FileReader(path))
+            .withCSVParser(parser)
+            .withSkipLines(1)
+            .build()
+            .map { it -> Player(
+                name = it[0],
+                team = Team (
+                    name = it[1],
+                    town = it[2]
+                ),
+                position = Position.valueOf(it[3]),
+                nationality = it[4],
+                agency = it[5],
+                transferCost = it[6].toInt(),
+                participations = it[7].toInt(),
+                goals = it[8].toInt(),
+                assists = it[9].toInt(),
+                yellowCards = it[10].toInt(),
+                redCards = it[11].toInt()
+            ) }
+            .toTypedArray()
+    }
+}
--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,53 @@
 package resolver
 
-class Resolver
+import model.Player
+import model.Team
+import model.enums.Position
+
+class Resolver(val players: Array<Player>) : IResolver {
+
+    init {
+        if (players.isEmpty())
+            throw Exception("Players list must have at least one element")
+    }
+
+    override fun getCountWithoutAgency(): Int {
+        return players.count { it.agency.isEmpty() }
+    }
+
+    override fun getBestScorerDefender(): Pair<String, Int> {
+        return players
+            .filter { it.position == Position.DEFENDER }
+            .maxByOrNull { it.goals }
+            ?.let { it.name to it.goals } ?: Pair("Неопределён", 0)
+    }
+
+    override fun getTheExpensiveGermanPlayerPosition(): String {
+        val position = players
+            .filter { it.nationality == "Germany" }
+            .maxByOrNull { it.transferCost }
+            ?.position
+        return when (position) {
+            Position.GOALKEEPER -> "Вратарь"
+            Position.DEFENDER -> "Защитник"
+            Position.MIDFIELD -> "Полузащитник"
+            Position.FORWARD -> "Нападющий"
+            else -> "Неопределеный"
+        }
+    }
+
+    override fun getTheRudestTeam(): Team {
+        return players
+            .groupBy { it.team }
+            .maxBy { it.value.map { p -> p.redCards }.average() }
+            .key
+    }
+
+    fun getStatistic(): List<Pair<Team, Int>> {
+        return players
+            .groupBy { it.team }
+            .map { it.key to it.value.map { p -> p.transferCost }.sum() }
+            .sortedBy { it.second }
+            .take(10)
+    }
+}
