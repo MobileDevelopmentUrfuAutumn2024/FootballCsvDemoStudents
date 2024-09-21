9807e04b170dc46213b03dc8a73d09ec8275718f
8CloudyCat8 <matveev2464@gmail.com>
2024-09-21 13:46:50 +0500

    Матвеев Евгений 4 вариант

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,35 @@
 package parser
 
-object CsvParser
+import model.Player
+import java.io.File
+
+object CsvParser {
+    fun parse(filePath: String): List<Player> {
+        val file = File(filePath)
+        val players = mutableListOf<Player>()
+
+        file.useLines { lines ->
+            val linesList = lines.toList()
+            linesList.first()
+            linesList.drop(1).forEach { line ->
+                val columns = line.split(';')
+                players.add(Player(
+                    name = columns[0],
+                    team = columns[1],
+                    city = columns[2],
+                    position = columns[3],
+                    nationality = columns[4],
+                    agency = columns[5].ifBlank { null },
+                    transferCost = columns[6].toInt(),
+                    participation = columns[7].toInt(),
+                    goals = columns[8].toInt(),
+                    assists = columns[9].toInt(),
+                    yellowCards = columns[10].toInt(),
+                    redCards = columns[11].toInt()
+                ))
+            }
+        }
+
+        return players
+    }
+}
