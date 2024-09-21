93780fb07a28c651993733609961f836c791600b
staylonely <dolganovnik20032906@gmail.com>
2024-09-21 10:15:17 +0500

    Долганов Никита первая команда решение

--- src/main/kotlin/model/Team.kt
+++ src/main/kotlin/model/Team.kt
@@ -1,3 +1,13 @@
 package model
 
-class Team
+class Team{
+    var teamName: String = "teamName"
+    var cityName: String = "cityName"
+
+
+    constructor(_teamName: String, _cityName: String){
+        teamName = _teamName
+        cityName = _cityName
+
+    }
+}
