7a5901281467f9e1c5fa8bc9dfbc6647dac71346
devofy <artemijv65@gmail.com>
2024-09-22 15:41:53 +0500

    Done

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,35 @@
-package parser
+import model.EPosition
+import model.Player
+import model.Team
+import java.io.File
 
-object CsvParser
+
+object CsvParser {
+    fun parse(filePath: String): List<Player> {
+        val players = mutableListOf<Player>()
+
+        File(filePath).useLines { lines ->
+            val linesList = lines.toList()
+            linesList.drop(1).forEach { line ->
+                val columns = line.split(';')
+                players.add(
+                    Player(
+                        name = columns[0],
+                        team = Team(name = columns[1], city = columns[2]),
+                        position = EPosition.entries.firstOrNull { it.name == columns[3] },
+                        nationality = columns[4],
+                        agency = columns[5].ifBlank { null },
+                        transferCost = columns[6].toIntOrNull(),
+                        participation = columns[7].toIntOrNull(),
+                        goals = columns[8].toIntOrNull(),
+                        assists = columns[9].toIntOrNull(),
+                        yellowCards = columns[10].toIntOrNull(),
+                        redCards = columns[11].toIntOrNull()
+                    )
+                )
+            }
+        }
+
+        return players
+    }
+}
--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,30 @@
 package resolver
 
-class Resolver
+import model.EPosition
+import model.Player
+import model.Team
+
+class Resolver(private val players: List<Player>) : IResolver {
+
+    override fun getCountWithoutAgency(): Int {
+        return players.count { it.agency.isNullOrEmpty() }
+    }
+
+    override fun getBestScorerDefender(): Pair<String, Int> {
+        return players.filter { it.position == EPosition.DEFENDER }
+            .maxByOrNull { it.goals ?: 0 }?.let { Pair(it.name, it.goals ?: 0) }
+            ?: throw NoSuchElementException("Нет защитников")
+    }
+
+    override fun getTheExpensiveGermanPlayerPosition(): String {
+        return players.filter { it.nationality == "Germany" }
+            .maxByOrNull { it.transferCost ?: 0 }?.position?.russianName
+            ?: throw NoSuchElementException("Нет немецких футболистов")
+    }
+
+    override fun getTheRudestTeam(): Team {
+        return players.groupBy { it.team }
+            .maxByOrNull { it.value.sumOf { it.redCards ?: 0 } / it.value.size }?.key
+            ?: throw NoSuchElementException("Не найдена хотя бы одна команда")
+    }
+}
