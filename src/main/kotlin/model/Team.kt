523b7c64cfb522fad0e0f783da055880dd2e895b
TIMERBOT1 <timbot_hovki@mail.ru>
2024-09-22 12:59:36 +0500

    Hot fix

--- src/main/kotlin/model/Team.kt
+++ src/main/kotlin/model/Team.kt
@@ -4,5 +4,6 @@ data class Team(
     val name: String,
     val city: String,
     var redCardsNum: Int = 0,
-    var playersCount: Int = 0
+    var playersCount: Int = 0,
+    var transferCost: Int = 0
 )
