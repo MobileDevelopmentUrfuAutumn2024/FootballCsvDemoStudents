918f10037ec492ad4bb512ac335ce6b46ef05243
Avrooid <102877744+Avrooid@users.noreply.github.com>
2024-09-23 06:10:37 +0500

    Задание 1. Бураков Никита

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
+import model.Player
+import parser.CsvParser
+import resolver.IResolver
+import resolver.Resolver
+import visualization.JFreeChartVisualizer
+
 fun main(args: Array<String>) {
-    print("Yeah rock!")
+    val players: List<Player> = CsvParser.parse("src/main/resources/fakePlayers.csv")
+    val resolver: IResolver = Resolver(players)
+    println("Количество игроков, интересы которых не представляет агенство: ${resolver.getCountWithoutAgency()}")
+    println("Автор наибольшего числа голов из числа защитников и их количество: ${resolver.getBestScorerDefender()}")
+    println("Русское название позиции самого дорогого немецкого игрока: ${resolver.getTheExpensiveGermanPlayerPosition()}")
+    println("Команда с наибольшим средним числом красных карточек на одного игрока: ${resolver.getTheRudestTeam()}")
+
+    val visualizer = JFreeChartVisualizer()
+    visualizer.showPositionsChart("Визуализация", resolver.getShareOfPlayersByPosition())
 }
