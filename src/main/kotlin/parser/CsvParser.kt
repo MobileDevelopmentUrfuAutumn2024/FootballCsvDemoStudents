e606d19923f3e33daaf626a443d6e0f8e6fef385
Roman <romamail.ru365@gmail.com>
2024-09-23 21:49:04 +0500

    Практика 1

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,51 @@
 package parser
 
-object CsvParser
+import java.io.File
+import model.Player
+import model.Team
+import java.util.Scanner
+
+object CsvParser {
+  private var players: MutableList<Player> = mutableListOf()
+
+  fun parse(path: String): List<Player> {
+    val file = File(path)
+    val scanner = Scanner(file)
+
+    scanner.nextLine()
+
+    while (scanner.hasNextLine()) {
+      parsePlayerFromString(scanner.nextLine())
+    }
+    return players
+  }
+
+  private fun parsePlayerFromString(playerString: String) {
+    val playerStringSplitted = playerString.split(';')
+
+    players.add(
+      Player(
+        name = playerStringSplitted[0],
+        team = Team(name = playerStringSplitted[1], city = playerStringSplitted[2]),
+        position = Player.Position.valueOf(playerStringSplitted[3]),
+        nationality = playerStringSplitted[4],
+        agency = playerStringSplitted[5],
+        transferCost = playerStringSplitted[6].toLongOrDefault(),
+        participations = playerStringSplitted[7].toIntOrDefault(),
+        goals = playerStringSplitted[8].toIntOrDefault(),
+        assists = playerStringSplitted[9].toIntOrDefault(),
+        yellowCards = playerStringSplitted[10].toIntOrDefault(),
+        redCards = playerStringSplitted[11].toIntOrDefault()
+      )
+    )
+  }
+
+  private fun String?.toIntOrDefault(default: Int = 0): Int {
+    return this?.toIntOrNull()?:default
+  }
+
+  private fun String?.toLongOrDefault(default: Long = 0): Long {
+    return this?.toLongOrNull()?:default
+  }
+
+}
