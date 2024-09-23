411a55cda1387c2731e90c415039f28f48ebf5fa
Maksiz03 <maksizkzkz03@gmail.com>
2024-09-24 00:48:46 +0500

    Update Resolver.kt

--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,42 @@
 package resolver
+import model.Player
+import model.Team
+import model.Position
 
-class Resolver
+class Resolver(private val players: List<Player>):IResolver {
+
+    override fun getCountWithoutAgency(): Int {
+        return players.count { it.agency == null || it.agency.isBlank() }
+    }
+
+    override fun getBestScorerDefender(): Pair<String, Int> {
+        val bestDefender = players
+            .filter { it.position == Position.DEFENDER }
+            .maxByOrNull { it.goalsCount }
+
+        
+        return bestDefender?.let { it.name to it.goalsCount } ?: ("" to 0)
+    }
+
+    override fun getTheExpensiveGermanPlayerPosition(): String {
+        val mostExpensiveGermanPlayer = players
+            .filter { it.nationality == "Germany" }
+            .maxByOrNull { it.transferCost }
+
+        return when (mostExpensiveGermanPlayer?.position) {
+            Position.GOALKEEPER -> "Вратарь"
+            Position.DEFENDER -> "Защитник"
+            Position.MIDFIELD -> "Полузащитник"
+            Position.FORWARD -> "Нападающий"
+            else -> "Неизвестно"
+        }
+    }
+
+    override fun getTheRudestTeam(): Team {
+        return players.groupBy { it.team }
+            .maxByOrNull { (_, teamPlayers) ->
+                teamPlayers.sumOf { it.redCardsCount }.toDouble() / teamPlayers.size
+            }?.key ?: players.first().team
+    }
+
+}
